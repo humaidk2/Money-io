dd8eb7133e0565149fe9dbc50945d2b43123267c
Humaid Khan <humaidk2@gmail.com>
2020-08-03 21:06:41 +0400

    Remove logs, comments, unused files

--- localstorage.js
+++ localstorage.js
@@ -1,8 +1,6 @@
 export const loadState = () => {
   try {
     const serializedState = localStorage.getItem("state");
-    console.log("loading");
-    console.log(serializedState);
     if (serializedState == null) {
       return undefined;
     }
@@ -14,8 +12,6 @@ export const loadState = () => {
 
 export const saveState = (state) => {
   try {
-    console.log("saving");
-    console.log(state);
     const serializedState = JSON.stringify(state);
     localStorage.setItem("state", serializedState);
   } catch (err) {
